c092ce1eface8e5cbddc258e3c14509d2a694f95
yumi-rakus <yumi.takahashi@rakus-partners.co.jp>
2020-06-17 12:32:52 +0900

    LoginForm.javaの作成

---
+++ src/main/java/jp/co/sample/form/LoginForm.java
@@ -0,0 +1,38 @@
+package jp.co.sample.form;
+
+public class LoginForm {
+
+	/**
+	 * メールアドレス
+	 */
+	private String mailAddress;
+
+	/**
+	 * パスワード
+	 */
+	private String password;
+
+	// getter setter
+	public String getMailAddress() {
+		return mailAddress;
+	}
+
+	public void setMailAddress(String mailAddress) {
+		this.mailAddress = mailAddress;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	// toString
+	@Override
+	public String toString() {
+		return "LoginForm [mailAddress=" + mailAddress + ", password=" + password + "]";
+	}
+
+}
