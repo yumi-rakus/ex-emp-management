288ab00add21f3402f115377acb32f0e11051690
yumi-rakus <yumi.takahashi@rakus-partners.co.jp>
2020-06-17 11:01:03 +0900

    Administrator.javaに引数のないコンストラクタを追加

--- src/main/java/jp/co/sample/domain/Administrator.java
+++ src/main/java/jp/co/sample/domain/Administrator.java
@@ -1,40 +1,42 @@
 package jp.co.sample.domain;
 
 public class Administrator {
-	
+
 	/**
 	 * 管理者情報を表すドメイン
+	 * 
 	 * @author takahashi yumi
 	 */
-	
-	
+
 	/**
 	 * ID
 	 */
 	private Integer id;
-	
+
 	/**
 	 * 名前
 	 */
 	private String name;
-	
+
 	/**
 	 * メールアドレス
 	 */
 	private String mailAddress;
-	
+
 	/**
 	 * パスワード
 	 */
 	private String password;
-	
-	
-	//Constructor
+
+	// Constructor
 	public Administrator(Integer id, String name, String mailAddress, String password) {
 		this.id = id;
 		this.name = name;
 		this.mailAddress = mailAddress;
 		this.password = password;
+	}
+
+	public Administrator() {
 	}
 
 	// getter setter
@@ -70,15 +72,12 @@ public class Administrator {
 		this.password = password;
 	}
 
-	
 	// toString
-	
+
 	@Override
 	public String toString() {
 		return "Administrator [id=" + id + ", name=" + name + ", mailAddress=" + mailAddress + ", password=" + password
 				+ "]";
 	}
-	
-	
 
 }
