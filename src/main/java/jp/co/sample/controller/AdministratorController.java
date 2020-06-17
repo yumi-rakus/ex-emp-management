4ad22b5c309ab3c00911c0a06601d89ee5476388
yumi-rakus <yumi.takahashi@rakus-partners.co.jp>
2020-06-17 16:56:54 +0900

    AdministratorController.javaに入力値チェック機能を追加

--- src/main/java/jp/co/sample/controller/AdministratorController.java
+++ src/main/java/jp/co/sample/controller/AdministratorController.java
@@ -5,6 +5,8 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -60,8 +62,12 @@ public class AdministratorController {
 	 * @return 「/」にリダイレクト
 	 */
 	@RequestMapping("/insert")
-	public String insert(InsertAdministratorForm form) {
-
+	public String insert(@Validated InsertAdministratorForm form, BindingResult result) {
+		
+		if(result.hasErrors()) {
+			return toInsert();
+		}
+		
 		Administrator administrator = new Administrator();
 
 		administrator.setName(form.getName());
@@ -95,6 +101,19 @@ public class AdministratorController {
 			session.setAttribute("administratorName", administrator.getName());
 			return "forward:/employee/showList";
 		}
+	}
+
+	/**
+	 * ログアウトする。
+	 * 
+	 * @return 「/」にリダイレクト
+	 */
+	@RequestMapping("/logout")
+	public String logout() {
+
+		session.invalidate();
+
+		return "redirect:/";
 	}
 
 }
