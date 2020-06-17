0c8a73138ec2dd57d63cc4cbea2702865ec37f63
yumi-rakus <yumi.takahashi@rakus-partners.co.jp>
2020-06-17 13:36:27 +0900

    AdministratorRepository.java / findByMailAddressAndPassword()のnull判定を修正

--- src/main/java/jp/co/sample/repository/AdministratorRepository.java
+++ src/main/java/jp/co/sample/repository/AdministratorRepository.java
@@ -1,6 +1,6 @@
 package jp.co.sample.repository;
 
-import java.util.Objects;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.RowMapper;
@@ -14,58 +14,60 @@ import jp.co.sample.domain.Administrator;
 
 @Repository
 public class AdministratorRepository {
-	
+
 	/**
 	 * Administratorオブジェクトを生成するRowMapper
 	 */
-	private static final RowMapper<Administrator> ADMINISTRATOR_ROW_MAPPER
-	= (rs, i) -> {
-		
+	private static final RowMapper<Administrator> ADMINISTRATOR_ROW_MAPPER = (rs, i) -> {
+
 		Administrator administrator = new Administrator();
-		
+
 		administrator.setId(rs.getInt("id"));
 		administrator.setName(rs.getString("name"));
 		administrator.setMailAddress(rs.getString("mail_address"));
 		administrator.setPassword(rs.getString("password"));
-		
+
 		return administrator;
 	};
-	
+
 	@Autowired
 	private NamedParameterJdbcTemplate template;
-	
-	
+
 	/**
 	 * 管理者情報を挿入する。
-	 * @param administrator　管理者情報
+	 * 
+	 * @param administrator 管理者情報
 	 */
 	public void insert(Administrator administrator) {
-		
+
 		SqlParameterSource param = new BeanPropertySqlParameterSource(administrator);
-		
+
 		String insertSql = "INSERT INTO administrators (name, mail_address, password) VALUES (:name, :mailAddress, :password)";
-		
+
 		template.update(insertSql, param);
-		
+
 	}
-	
+
 	/**
 	 * メールアドレスとパスワードから管理者情報を取得する（存在しない場合はnullを返す）。
 	 * 
-	 * @param mailAddress　メールアドレス
-	 * @param password　パスワード
-	 * @return　検索された管理者情報
+	 * @param mailAddress メールアドレス
+	 * @param password    パスワード
+	 * @return 検索された管理者情報
 	 */
 	public Administrator findByMailAddressAndPassword(String mailAddress, String password) {
-		
+
 		String sql = "SELECT * FROM administrators WHERE mail_address = :mailAddress AND password = :password";
-		
-		SqlParameterSource param = new MapSqlParameterSource().addValue("mailAddress", mailAddress).addValue("password", password);
-		
-		if(Objects.isNull(template.queryForObject(sql, param, ADMINISTRATOR_ROW_MAPPER))) {
+
+		SqlParameterSource param = new MapSqlParameterSource().addValue("mailAddress", mailAddress).addValue("password",
+				password);
+
+		List<Administrator> adminList = template.query(sql, param, ADMINISTRATOR_ROW_MAPPER);
+
+		if (adminList.size() == 0) {
 			return null;
 		} else {
-			Administrator administrator = template.queryForObject(sql, param, ADMINISTRATOR_ROW_MAPPER);
+			Administrator administrator = adminList.get(0);
 			return administrator;
 		}
 	}
