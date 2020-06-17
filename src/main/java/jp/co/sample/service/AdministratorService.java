dcc898ccff94085876e794c4df5d4bd70dc01928
yumi-rakus <yumi.takahashi@rakus-partners.co.jp>
2020-06-17 12:28:34 +0900

    AdministratorService.javaの作成

---
+++ src/main/java/jp/co/sample/service/AdministratorService.java
@@ -0,0 +1,26 @@
+package jp.co.sample.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import jp.co.sample.domain.Administrator;
+import jp.co.sample.repository.AdministratorRepository;
+
+@Service
+@Transactional
+public class AdministratorService {
+
+	@Autowired
+	private AdministratorRepository administratorRepository;
+	
+	/**
+	 * 管理者情報を挿入する。
+	 * 
+	 * @param administrator　挿入したい管理者情報
+	 */
+	public void insert(Administrator administrator) {
+		administratorRepository.insert(administrator);
+	}
+
+}
