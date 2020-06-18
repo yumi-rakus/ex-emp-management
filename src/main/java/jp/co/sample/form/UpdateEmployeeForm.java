cf976ee8f9de752be62c3f5708ca6761d4613df8
yumi-rakus <yumi.takahashi@rakus-partners.co.jp>
2020-06-18 15:16:21 +0900

    UpdateEmployeeForm.javaにすべてのフィールドに入力値チェック機能を追加

--- src/main/java/jp/co/sample/form/UpdateEmployeeForm.java
+++ src/main/java/jp/co/sample/form/UpdateEmployeeForm.java
@@ -1,7 +1,9 @@
 package jp.co.sample.form;
 
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
+
 
 public class UpdateEmployeeForm {
 
@@ -13,6 +15,7 @@ public class UpdateEmployeeForm {
 	/**
 	 * 名前
 	 */
+	@NotBlank(message = "名前を入力してください")
 	private String name;
 
 	/**
@@ -23,48 +26,74 @@ public class UpdateEmployeeForm {
 	/**
 	 * 性別
 	 */
+	@NotBlank(message = "性別を選んでください")
 	private String gender;
 
 	/**
-	 * 入社日
+	 * 入社日（年）
 	 */
-	private java.util.Date hireDate;
+	@Pattern(regexp = "[1-2][0-9]{3}", message = "年：半角数字4桁を入力してください")
+	@NotBlank(message = "年を入力してください")
+	private String hireYear;
+
+	/**
+	 * 入社日（月）
+	 */
+	@Pattern(regexp = "[1-9]|1[0-2]", message = "月：1~12の半角数字を入力してください")
+	@NotBlank(message = "月を入力してください")
+	private String hireMonth;
+
+	/**
+	 * 入社日（日）
+	 */
+	@Pattern(regexp = "[1-9]|[12][0-9]|3[01]", message = "日：1~31の半角数字を入力してください")
+	@NotBlank(message = "日を入力してください")
+	private String hireDay;
 
 	/**
 	 * メールアドレス
 	 */
+	@NotBlank(message="メールアドレスを入力してください")
+	@Email(message="Emailの形式が不正です")
 	private String mailAddress;
 
 	/**
 	 * 郵便番号
 	 */
+	@Pattern(regexp="^[0-9]{3}-[0-9]{4}$", message="(半角数字3桁)-(半角数字4桁)で入力してください")
 	private String zipCode;
 
 	/**
 	 * 住所
 	 */
+	@NotBlank(message="住所を入力してください")
 	private String address;
 
 	/**
 	 * 電話番号
 	 */
+	@Pattern(regexp="^0[789]0-[0-9]{4}-[0-9]{4}$", message="(半角数字3桁)-(半角数字4桁)で入力してください")
 	private String telephone;
 
 	/**
 	 * 給料
 	 */
+	@NotBlank(message = "給料を入力してください")
+	@Pattern(regexp = "[0-9]+", message = "半角数字を入力してください")
 	private String salary;
 
 	/**
 	 * 特性
 	 */
+	@NotBlank(message = "特性を入力してください")
 	private String characteristics;
 
 	/**
 	 * 扶養人数
 	 */
-	@NotBlank(message="扶養人数を入力してください")
-	@Pattern(regexp="[0-9]+", message="半角数字を入力してください")
+
+	@Pattern(regexp = "[0-9]+", message = "半角数字を入力してください")
+	@NotBlank(message = "扶養人数を入力してください")
 	private String dependentsCount;
 
 	// getter setter
@@ -98,14 +127,6 @@ public class UpdateEmployeeForm {
 
 	public void setGender(String gender) {
 		this.gender = gender;
-	}
-
-	public java.util.Date getHireDate() {
-		return hireDate;
-	}
-
-	public void setHireDate(java.util.Date hireDate) {
-		this.hireDate = hireDate;
 	}
 
 	public String getMailAddress() {
@@ -164,13 +185,38 @@ public class UpdateEmployeeForm {
 		this.dependentsCount = dependentsCount;
 	}
 
+	public String getHireYear() {
+		return hireYear;
+	}
+
+	public void setHireYear(String hireYear) {
+		this.hireYear = hireYear;
+	}
+
+	public String getHireMonth() {
+		return hireMonth;
+	}
+
+	public void setHireMonth(String hireMonth) {
+		this.hireMonth = hireMonth;
+	}
+
+	public String getHireDay() {
+		return hireDay;
+	}
+
+	public void setHireDay(String hireDay) {
+		this.hireDay = hireDay;
+	}
+
 	// toString
 	@Override
 	public String toString() {
 		return "UpdateEmployeeForm [id=" + id + ", name=" + name + ", image=" + image + ", gender=" + gender
-				+ ", hireDate=" + hireDate + ", mailAddress=" + mailAddress + ", zipCode=" + zipCode + ", address="
-				+ address + ", telephone=" + telephone + ", salary=" + salary + ", characteristics=" + characteristics
-				+ ", dependentsCount=" + dependentsCount + "]";
+				+ ", hireYear=" + hireYear + ", hireMonth=" + hireMonth + ", hireDay=" + hireDay + ", mailAddress="
+				+ mailAddress + ", zipCode=" + zipCode + ", address=" + address + ", telephone=" + telephone
+				+ ", salary=" + salary + ", characteristics=" + characteristics + ", dependentsCount=" + dependentsCount
+				+ "]";
 	}
 
 }
