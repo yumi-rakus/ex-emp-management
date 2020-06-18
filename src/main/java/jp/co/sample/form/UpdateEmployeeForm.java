24f3679034698da33d266c868ef659d3412e68e7
yumi-rakus <yumi.takahashi@rakus-partners.co.jp>
2020-06-18 10:37:13 +0900

    UpdateEmployeeForm.javaにEmployee.javaと同じフィールドを定義

--- src/main/java/jp/co/sample/form/UpdateEmployeeForm.java
+++ src/main/java/jp/co/sample/form/UpdateEmployeeForm.java
@@ -1,15 +1,70 @@
 package jp.co.sample.form;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
 public class UpdateEmployeeForm {
 
 	/**
-	 * 従業員ID
+	 * ID
 	 */
 	private String id;
 
 	/**
+	 * 名前
+	 */
+	private String name;
+
+	/**
+	 * 画像
+	 */
+	private String image;
+
+	/**
+	 * 性別
+	 */
+	private String gender;
+
+	/**
+	 * 入社日
+	 */
+	private java.util.Date hireDate;
+
+	/**
+	 * メールアドレス
+	 */
+	private String mailAddress;
+
+	/**
+	 * 郵便番号
+	 */
+	private String zipCode;
+
+	/**
+	 * 住所
+	 */
+	private String address;
+
+	/**
+	 * 電話番号
+	 */
+	private String telephone;
+
+	/**
+	 * 給料
+	 */
+	private String salary;
+
+	/**
+	 * 特性
+	 */
+	private String characteristics;
+
+	/**
 	 * 扶養人数
 	 */
+	@NotBlank(message="扶養人数を入力してください")
+	@Pattern(regexp="[0-9]+", message="半角数字を入力してください")
 	private String dependentsCount;
 
 	// getter setter
@@ -19,6 +74,86 @@ public class UpdateEmployeeForm {
 
 	public void setId(String id) {
 		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getImage() {
+		return image;
+	}
+
+	public void setImage(String image) {
+		this.image = image;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public java.util.Date getHireDate() {
+		return hireDate;
+	}
+
+	public void setHireDate(java.util.Date hireDate) {
+		this.hireDate = hireDate;
+	}
+
+	public String getMailAddress() {
+		return mailAddress;
+	}
+
+	public void setMailAddress(String mailAddress) {
+		this.mailAddress = mailAddress;
+	}
+
+	public String getZipCode() {
+		return zipCode;
+	}
+
+	public void setZipCode(String zipCode) {
+		this.zipCode = zipCode;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getTelephone() {
+		return telephone;
+	}
+
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+
+	public String getSalary() {
+		return salary;
+	}
+
+	public void setSalary(String salary) {
+		this.salary = salary;
+	}
+
+	public String getCharacteristics() {
+		return characteristics;
+	}
+
+	public void setCharacteristics(String characteristics) {
+		this.characteristics = characteristics;
 	}
 
 	public String getDependentsCount() {
@@ -32,7 +167,10 @@ public class UpdateEmployeeForm {
 	// toString
 	@Override
 	public String toString() {
-		return "UpdateEmployeeForm [id=" + id + ", dependentsCount=" + dependentsCount + "]";
+		return "UpdateEmployeeForm [id=" + id + ", name=" + name + ", image=" + image + ", gender=" + gender
+				+ ", hireDate=" + hireDate + ", mailAddress=" + mailAddress + ", zipCode=" + zipCode + ", address="
+				+ address + ", telephone=" + telephone + ", salary=" + salary + ", characteristics=" + characteristics
+				+ ", dependentsCount=" + dependentsCount + "]";
 	}
 
 }
